a794e4500681d04f8789f07551f7e65b52412d0b
Eric Yu <eric@distyl.ai>
2026-08-06 22:45:39 -0400

    refactor: place the Duhamel formula under analysis

--- TauCeti/Analysis/SpecialFunctions/Exponential.lean
+++ TauCeti/Analysis/SpecialFunctions/Exponential.lean
@@ -5,8 +5,8 @@ Released under Apache 2.0 license as described in the file LICENSE.
 module
 
 public import Mathlib.Analysis.SpecialFunctions.Exponential
-public import Mathlib.Analysis.Calculus.Deriv.Mul
-public import Mathlib.Analysis.Calculus.Deriv.Shift
+import Mathlib.Analysis.Calculus.Deriv.Mul
+import Mathlib.Analysis.Calculus.Deriv.Shift
 public import Mathlib.MeasureTheory.Integral.IntervalIntegral.FundThmCalculus
 
 /-!
